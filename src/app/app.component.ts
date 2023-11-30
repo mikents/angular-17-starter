e5d68a0b29f74ec400a79f5a8d46321371f96ff6
mikents <mike@nts-apps.com>
2023-11-30 13:45:29 -0500

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface TestData {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -9,6 +18,17 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'angular-17-starter';
+export class AppComponent implements OnInit {
+  
+  testData$: Observable<TestData[]>;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    
+  }
+
+  getData()
 }
